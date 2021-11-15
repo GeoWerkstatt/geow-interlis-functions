4b72db9c1f68927e0be600619e8cda8aa3b301a9
Philipp Lüthi <philipp.luethi@geowerkstatt.ch>
2021-11-15 10:42:07 +0100

    Implement InterlisFuncion interface

--- src/main/java/ch/geowerkstatt/ilivalidator/extensions/functions/GetLengthFunction.java
+++ src/main/java/ch/geowerkstatt/ilivalidator/extensions/functions/GetLengthFunction.java
@@ -1,4 +1,31 @@
 package ch.geowerkstatt.ilivalidator.extensions.functions;
 
-public class GetLengthFunction {
+import ch.ehi.basics.settings.Settings;
+import ch.interlis.ili2c.metamodel.TransferDescription;
+import ch.interlis.iom.IomObject;
+import ch.interlis.iox.IoxValidationConfig;
+import ch.interlis.iox_j.logging.LogEventFactory;
+import ch.interlis.iox_j.validator.InterlisFunction;
+import ch.interlis.iox_j.validator.ObjectPool;
+import ch.interlis.iox_j.validator.Value;
+
+public class GetLengthFunction implements InterlisFunction {
+    @Override
+    public void init(TransferDescription transferDescription,
+                     Settings settings,
+                     IoxValidationConfig ioxValidationConfig,
+                     ObjectPool objectPool,
+                     LogEventFactory logEventFactory) {
+
+    }
+
+    @Override
+    public Value evaluate(String s, String s1, IomObject iomObject, Value[] values) {
+        return null;
+    }
+
+    @Override
+    public String getQualifiedIliName() {
+        return "GeoW_FunctionsExt.GetLength";
+    }
 }
