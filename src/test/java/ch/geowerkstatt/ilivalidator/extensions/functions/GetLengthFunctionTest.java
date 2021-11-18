bb86a3ece8b40e5cc71d9eb5f3f5406be825a270
Philipp Luethi <philipp.luethi@geowerkstatt.ch>
2021-11-18 08:52:37 +0100

    Add GetLength test

---
+++ src/test/java/ch/geowerkstatt/ilivalidator/extensions/functions/GetLengthFunctionTest.java
@@ -0,0 +1,20 @@
+package ch.geowerkstatt.ilivalidator.extensions.functions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GetLengthFunctionTest {
+
+    ValidationTestHelper vh = null;
+    @BeforeEach
+    void setUp() {
+        vh = new ValidationTestHelper();
+    }
+
+    @Test
+    void MandatoryConstraintOnThis(){
+
+    }
+}
