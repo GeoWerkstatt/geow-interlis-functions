a586274d505ed54252f9be9f8aeafb639c097c70
Philipp Luethi <philipp.luethi@geowerkstatt.ch>
2021-12-02 10:28:37 +0100

    Refactor functions

--- src/main/java/ch/geowerkstatt/ilivalidator/extensions/functions/GetAreaFunction.java
+++ src/main/java/ch/geowerkstatt/ilivalidator/extensions/functions/GetAreaFunction.java
@@ -1,123 +1,60 @@
 package ch.geowerkstatt.ilivalidator.extensions.functions;
 
-import ch.ehi.basics.logging.EhiLogger;
-import ch.ehi.basics.settings.Settings;
-import ch.interlis.ili2c.Ili2cException;
-import ch.interlis.ili2c.metamodel.Element;
-import ch.interlis.ili2c.metamodel.ObjectPath;
 import ch.interlis.ili2c.metamodel.PathEl;
-import ch.interlis.ili2c.metamodel.TransferDescription;
 import ch.interlis.ili2c.metamodel.Viewable;
 import ch.interlis.iom.IomObject;
 import ch.interlis.iox.IoxException;
-import ch.interlis.iox.IoxValidationConfig;
 import ch.interlis.iox_j.jts.Iox2jtsext;
-import ch.interlis.iox_j.logging.LogEventFactory;
-import ch.interlis.iox_j.validator.InterlisFunction;
-import ch.interlis.iox_j.validator.ObjectPool;
-import ch.interlis.iox_j.validator.Validator;
 import ch.interlis.iox_j.validator.Value;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 
-public class GetAreaFunction implements InterlisFunction {
-
-    private LogEventFactory logger;
-    private TransferDescription td;
-    private Validator validator;
+public class GetAreaFunction extends BaseInterlisFunction {
 
     @Override
-    public void init(TransferDescription td, Settings settings, IoxValidationConfig validationConfig, ObjectPool objectPool, LogEventFactory logEventFactory) {
-        this.logger = logEventFactory;
-        this.logger.setValidationConfig(validationConfig);
-        this.td = td;
-        this.validator = (Validator) settings.getTransientObject(IOX_VALIDATOR);
+    public String getQualifiedIliName() {
+        return "GeoW_FunctionsExt.GetArea";
+    }
+
+    private double getArea(IomObject surface) {
+        try {
+            return Iox2jtsext.surface2JTS(surface, 0.0).getArea();
+        } catch (IoxException ex) {
+            logger.addEvent(logger.logErrorMsg("Could not calculate {0}", this.getQualifiedIliName()));
+        }
+        return 0.0;
     }
 
     @Override
-    public Value evaluate(String validationKind, String usageScope, IomObject contextObject, Value[] arguments) {
+    public Value evaluateInternal(String validationKind, String usageScope, IomObject contextObject, Value[] arguments) {
         Value argObjects = arguments[0];
         Value argPath = arguments[1];
 
-        if (argObjects.skipEvaluation()) {
-            return argObjects;
-        }
-        if (argPath.skipEvaluation()) {
-            return argPath;
-        }
         if (argObjects.isUndefined()) {
             return Value.createSkipEvaluation();
         }
+
         if (argObjects.getComplexObjects() == null) {
             return Value.createUndefined();
         }
 
         Collection<IomObject> surfaces = new ArrayList<>();
-        Viewable contextClass;
 
         if (argPath.isUndefined()) {
             surfaces.addAll(argObjects.getComplexObjects());
         } else {
-            contextClass = getContextClass(contextObject, argObjects);
+            Viewable contextClass = EvaluationHelper.getContextClass(td, contextObject, argObjects);
 
             if (contextClass == null) {
                 throw new IllegalStateException("unknown class in " + usageScope);
             }
 
-            PathEl[] attributePath = getAttributePathEl(contextClass, argPath);
-
-            for (IomObject rootObject : argObjects.getComplexObjects()) {
-                Value surfaceAttributes = validator.getValueFromObjectPath(null, rootObject, attributePath, null);
-                if (!(surfaceAttributes.isUndefined() || surfaceAttributes.skipEvaluation() || surfaceAttributes.getComplexObjects() == null)) {
-                    surfaces.addAll(surfaceAttributes.getComplexObjects());
-                }
-            }
+            PathEl[] attributePath = EvaluationHelper.getAttributePathEl(validator, contextClass, argPath);
+            surfaces.addAll(EvaluationHelper.evaluateAttributes(validator, argObjects, attributePath));
         }
 
-        double result = 0.0d;
-
-        for (IomObject surface : surfaces) {
-            try {
-                result += Iox2jtsext.surface2JTS(surface, 0.0).getArea();
-            } catch (IoxException ex) {
-                logger.addEvent(logger.logErrorMsg("Could not calculate GetArea for Object {0}", surface.toString()));
-            }
-        }
-
-        return new Value(result);
-    }
-
-    private PathEl[] getAttributePathEl(Viewable<Element> contextClass, Value argPath) {
-        try {
-            ObjectPath objectPath = validator.parseObjectOrAttributePath(contextClass, argPath.getValue());
-            if (objectPath.getPathElements() != null) {
-                return objectPath.getPathElements();
-            }
-        } catch (Ili2cException e) {
-            EhiLogger.logError(e);
-        }
-        return null;
-    }
-
-    private Viewable getContextClass(IomObject iomObject, Value argObjects) {
-        if (iomObject != null) {
-            return (Viewable) td.getElement(iomObject.getobjecttag());
-        } else if (argObjects.getViewable() != null) {
-            return argObjects.getViewable();
-        } else if (argObjects.getComplexObjects() != null) {
-            Iterator<IomObject> it = argObjects.getComplexObjects().iterator();
-            if (!it.hasNext()) {
-                return null;
-            }
-            return (Viewable) td.getElement(it.next().getobjecttag());
-        }
-        return null;
-    }
-
-    @Override
-    public String getQualifiedIliName() {
-        return "GeoW_FunctionsExt.GetArea";
+        double areaSum = EvaluationHelper.sum(surfaces, this::getArea);
+        return new Value(areaSum);
     }
 }
--- src/main/java/ch/geowerkstatt/ilivalidator/extensions/functions/GetLengthFunction.java
+++ src/main/java/ch/geowerkstatt/ilivalidator/extensions/functions/GetLengthFunction.java
@@ -1,123 +1,60 @@
 package ch.geowerkstatt.ilivalidator.extensions.functions;
 
-import ch.ehi.basics.logging.EhiLogger;
-import ch.ehi.basics.settings.Settings;
-import ch.interlis.ili2c.Ili2cException;
-import ch.interlis.ili2c.metamodel.Element;
-import ch.interlis.ili2c.metamodel.ObjectPath;
 import ch.interlis.ili2c.metamodel.PathEl;
-import ch.interlis.ili2c.metamodel.TransferDescription;
 import ch.interlis.ili2c.metamodel.Viewable;
 import ch.interlis.iom.IomObject;
 import ch.interlis.iox.IoxException;
-import ch.interlis.iox.IoxValidationConfig;
 import ch.interlis.iox_j.jts.Iox2jtsext;
-import ch.interlis.iox_j.logging.LogEventFactory;
-import ch.interlis.iox_j.validator.InterlisFunction;
-import ch.interlis.iox_j.validator.ObjectPool;
-import ch.interlis.iox_j.validator.Validator;
 import ch.interlis.iox_j.validator.Value;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 
-public class GetLengthFunction implements InterlisFunction {
-
-    private LogEventFactory logger;
-    private TransferDescription td;
-    private Validator validator;
+public class GetLengthFunction extends BaseInterlisFunction {
 
     @Override
-    public void init(TransferDescription td, Settings settings, IoxValidationConfig validationConfig, ObjectPool objectPool, LogEventFactory logEventFactory) {
-        this.logger = logEventFactory;
-        this.logger.setValidationConfig(validationConfig);
-        this.td = td;
-        this.validator = (Validator) settings.getTransientObject(IOX_VALIDATOR);
+    public String getQualifiedIliName() {
+        return "GeoW_FunctionsExt.GetLength";
+    }
+
+    private double getLength(IomObject polyline) {
+        try {
+            return Iox2jtsext.polyline2JTS(polyline, false, 0.0).getLength();
+        } catch (IoxException ex) {
+            logger.addEvent(logger.logErrorMsg("Could not calculate {0}", this.getQualifiedIliName()));
+        }
+        return 0.0;
     }
 
     @Override
-    public Value evaluate(String validationKind, String usageScope, IomObject contextObject, Value[] arguments) {
+    public Value evaluateInternal(String validationKind, String usageScope, IomObject contextObject, Value[] arguments) {
         Value argObjects = arguments[0];
         Value argPath = arguments[1];
 
-        if (argObjects.skipEvaluation()) {
-            return argObjects;
-        }
-        if (argPath.skipEvaluation()) {
-            return argPath;
-        }
         if (argObjects.isUndefined()) {
             return Value.createSkipEvaluation();
         }
+
         if (argObjects.getComplexObjects() == null) {
             return Value.createUndefined();
         }
 
-        Collection<IomObject> polylines = new ArrayList<>();
-        Viewable contextClass;
+        Collection<IomObject> surfaces = new ArrayList<>();
 
         if (argPath.isUndefined()) {
-            polylines.addAll(argObjects.getComplexObjects());
+            surfaces.addAll(argObjects.getComplexObjects());
         } else {
-            contextClass = getContextClass(contextObject, argObjects);
+            Viewable contextClass = EvaluationHelper.getContextClass(td, contextObject, argObjects);
 
             if (contextClass == null) {
                 throw new IllegalStateException("unknown class in " + usageScope);
             }
 
-            PathEl[] polylineAttributePath = getAttributePathEl(contextClass, argPath);
-
-            for (IomObject rootObject : argObjects.getComplexObjects()) {
-                Value polylineAttributes = validator.getValueFromObjectPath(null, rootObject, polylineAttributePath, null);
-                if (!(polylineAttributes.isUndefined() || polylineAttributes.skipEvaluation() || polylineAttributes.getComplexObjects() == null)) {
-                    polylines.addAll(polylineAttributes.getComplexObjects());
-                }
-            }
+            PathEl[] attributePath = EvaluationHelper.getAttributePathEl(validator, contextClass, argPath);
+            surfaces.addAll(EvaluationHelper.evaluateAttributes(validator, argObjects, attributePath));
         }
 
-        double result = 0.0d;
-
-        for (IomObject polyline : polylines) {
-            try {
-                result += Iox2jtsext.polyline2JTS(polyline, false, 0.0).getLength();
-            } catch (IoxException ex) {
-                logger.addEvent(logger.logErrorMsg("Could not calculate GetLength for Object {0}", polyline.toString()));
-            }
-        }
-
-        return new Value(result);
-    }
-
-    private PathEl[] getAttributePathEl(Viewable<Element> contextClass, Value argPath) {
-        try {
-            ObjectPath objectPath = validator.parseObjectOrAttributePath(contextClass, argPath.getValue());
-            if (objectPath.getPathElements() != null) {
-                return objectPath.getPathElements();
-            }
-        } catch (Ili2cException e) {
-            EhiLogger.logError(e);
-        }
-        return null;
-    }
-
-    private Viewable getContextClass(IomObject iomObject, Value argObjects) {
-        if (iomObject != null) {
-            return (Viewable) td.getElement(iomObject.getobjecttag());
-        } else if (argObjects.getViewable() != null) {
-            return argObjects.getViewable();
-        } else if (argObjects.getComplexObjects() != null) {
-            Iterator<IomObject> it = argObjects.getComplexObjects().iterator();
-            if (!it.hasNext()) {
-                return null;
-            }
-            return (Viewable) td.getElement(it.next().getobjecttag());
-        }
-        return null;
-    }
-
-    @Override
-    public String getQualifiedIliName() {
-        return "GeoW_FunctionsExt.GetLength";
+        double lengthSum = EvaluationHelper.sum(surfaces, this::getLength);
+        return new Value(lengthSum);
     }
 }
