f5e7d6d9732c7a19a74b1c6a0d51006368744f7b
Dominic Burger <dominic.burger@geowerkstatt.ch>
2023-12-20 15:46:02 +0100

    Use default node capacity for STRtree

--- src/main/java/ch/geowerkstatt/ilivalidator/extensions/functions/PolylinesOverlapIoxPlugin.java
+++ src/main/java/ch/geowerkstatt/ilivalidator/extensions/functions/PolylinesOverlapIoxPlugin.java
@@ -89,7 +89,7 @@ public final class PolylinesOverlapIoxPlugin extends BaseInterlisFunction {
             return false;
         }
 
-        STRtree tree = new STRtree(lines.size());
+        STRtree tree = new STRtree();
         for (CompoundCurve line : lines) {
             tree.insert(line.getEnvelopeInternal(), line);
         }
