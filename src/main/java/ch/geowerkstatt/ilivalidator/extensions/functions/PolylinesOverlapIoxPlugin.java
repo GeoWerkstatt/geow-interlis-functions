428bbc5a69a5b30b92468b515a5c78ce905ddd22
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2024-12-11 11:48:41 +0100

    Add PolylinesOverlap test

--- src/main/java/ch/geowerkstatt/ilivalidator/extensions/functions/PolylinesOverlapIoxPlugin.java
+++ src/main/java/ch/geowerkstatt/ilivalidator/extensions/functions/PolylinesOverlapIoxPlugin.java
@@ -55,6 +55,7 @@ public final class PolylinesOverlapIoxPlugin extends BaseInterlisFunction {
         HasEqualLinePartKey key = new HasEqualLinePartKey(objectIds, argPath.isUndefined() ? null : argPath.getValue());
 
         boolean hasOverlap = HAS_EQUAL_LINE_PART_CACHE.computeIfAbsent(key, k -> {
+            logger.addEvent(logger.logDetailInfoMsg("Calculating {0} for key {1}", getQualifiedIliName(), k.toString()));
             List<CompoundCurve> lines = convertToJTSLines(polylineObjects);
             return hasEqualLinePart(lines);
         });
@@ -131,6 +132,14 @@ public final class PolylinesOverlapIoxPlugin extends BaseInterlisFunction {
         @Override
         public int hashCode() {
             return Objects.hash(objectIds, attributeName);
+        }
+
+        @Override
+        public String toString() {
+            return "HasEqualLinePartKey{"
+                    + "objectIds=[" + String.join(", ", objectIds)
+                    + "], attributeName='" + attributeName + '\''
+                    + '}';
         }
     }
 }
--- src/test/java/ch/geowerkstatt/ilivalidator/extensions/functions/PolylinesOverlapIoxPluginTest.java
+++ src/test/java/ch/geowerkstatt/ilivalidator/extensions/functions/PolylinesOverlapIoxPluginTest.java
@@ -1,7 +1,9 @@
 package ch.geowerkstatt.ilivalidator.extensions.functions;
 
+import ch.ehi.basics.logging.EhiLogger;
 import ch.interlis.ili2c.Ili2cFailure;
 import ch.interlis.iox.IoxException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +12,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public final class PolylinesOverlapIoxPluginTest {
     private static final String TEST_DATA = "PolylinesOverlap/TestData.xtf";
     private static final String TEST_DATA_SAME_LINE = "PolylinesOverlap/TestDataSameLine.xtf";
+    private static final String TEST_DATA_ONLY_T2 = "PolylinesOverlap/TestDataOnlyT2.xtf";
     private ValidationTestHelper vh = null;
+
+    @BeforeAll
+    static void init() {
+        EhiLogger.getInstance().setTraceFilter(false);
+    }
 
     @BeforeEach
     void setUp() {
@@ -37,6 +45,18 @@ public final class PolylinesOverlapIoxPluginTest {
         AssertionHelper.assertConstraintErrors(vh, 1, "setConstraintAllNoOverlaps");
         AssertionHelper.assertConstraintErrors(vh, 1, "setConstraintT1");
         AssertionHelper.assertConstraintErrors(vh, 1, "setConstraintT2");
+        AssertionHelper.assertConstraintErrors(vh, 1, "setConstraintT3");
+        AssertionHelper.assertNoConstraintError(vh, "setConstraintT1or2"); // One line in T1 is a segment of T2
+        AssertionHelper.assertNoConstraintError(vh, "setConstraintT2or3"); // The lines in T2 and T3 are equal
+    }
+
+    @Test
+    void setConstraintEverythingFilteredOut() throws Ili2cFailure, IoxException {
+        vh.runValidation(new String[]{TEST_DATA_ONLY_T2}, new String[]{"PolylinesOverlap/PolylinesOverlap.ili"});
+        assertEquals(3, vh.getErrs().size());
+        AssertionHelper.assertConstraintErrors(vh, 1, "setConstraintAllNoOverlaps");
+        AssertionHelper.assertConstraintErrors(vh, 1, "setConstraintT1");
+        AssertionHelper.assertNoConstraintError(vh, "setConstraintT2");
         AssertionHelper.assertConstraintErrors(vh, 1, "setConstraintT3");
         AssertionHelper.assertNoConstraintError(vh, "setConstraintT1or2"); // One line in T1 is a segment of T2
         AssertionHelper.assertNoConstraintError(vh, "setConstraintT2or3"); // The lines in T2 and T3 are equal
